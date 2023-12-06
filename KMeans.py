e6eb3677563deb91225c06d368eca4697e7e98d9
Kevahlol <62565506+Kevahlol@users.noreply.github.com>
2023-12-05 20:34:33 -0600

    Update KMeans.py

--- KMeans.py
+++ KMeans.py
@@ -1,4 +1,3 @@
-
 from sklearn.cluster import KMeans
 import numpy as np
 
@@ -19,13 +18,14 @@ class KMeansClustering:
 
 if __name__ == "__main__":
     
-    #Conjuntoi para entrenamiento
+    #Conjuntoi para entrenamiento, ingresos y gastos en MXN, y nivel educativo. 0 = Primaria, 1 = Secundaria, 2 = Preparatoria, 3 = Universidad
     X_train = np.array([
-        [160, 60], [175, 70], [155, 50], [180, 80], [165, 65],
-        [150, 45], [185, 90], [170, 75], [140, 40], [195, 100],
-        [180, 70], [165, 50], [190, 85], [175, 65], [200, 110],
-        [155, 60], [180, 90], [170, 65], [185, 95], [175, 80],
-        [125, 25], [130, 30], [110, 22], [140, 18], [135, 27]
+        [15000, 12000, 1], [50000, 40000, 3], [33000, 32000, 2], [15000, 12000, 0], [70000, 45000, 3],
+        [28000, 20000, 2], [85000, 60000, 3], [19000, 15000, 1], [7000, 6000, 0], [90000, 70000, 3],
+        [40000, 33000, 2], [75000, 50000, 3], [35000, 20000, 2], [100000, 80000, 3], [26000, 26000, 1], 
+        [39000, 35000, 2], [20000, 18000, 1], [85000, 55000, 3], [75000, 55000, 3], [9000, 8000, 0],
+        [11000, 8000, 0], [15000, 15000, 0], [17000, 17000, 1], [33000, 24000, 2], [36000, 32000, 2],
+        [20000, 16000, 1], [8000, 8000, 0], [90000, 60000, 3], [31000, 28000, 2], [24000, 23000, 1]
     ])
 
     #
@@ -37,7 +37,12 @@ if __name__ == "__main__":
     #Clasificación de las instancias en los clústers
     predictions = kmeans_model.predict(X_train)
 
-    #Muestreo de resultados
+    #Muestreo de resultados por instancias
     print("Predicciones de clúster para el conjunto de entrenamiento:")
     for i, pred in enumerate(predictions):
         print(f"Instancia {i + 1}: Clúster {pred}")
+
+    #Muestreo de resultados por clúster
+    for cluster in range(kmeans_model.n_clusters):
+        instances_in_cluster = X_train[predictions == cluster]
+        print(f"\nInstancias en el Cluster {cluster}:\n{instances_in_cluster}")
